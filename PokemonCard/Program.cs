5a026ae3772230d6fe89ab0d86aa87a1b6703e5c
Perry5124 <perry147258369@gmail.com>
2026-08-20 14:46:00 +0800

    feat: 新增管理員後台通用版型 (_AdminLayout)

--- PokemonCard/Program.cs
+++ PokemonCard/Program.cs
@@ -6,13 +6,15 @@ var builder = WebApplication. CreateBuilder(args);
 // Add services to the container.
 builder. Services. AddControllersWithViews( );
 
-var app = builder. Build( );
 
-builder. Services. AddDbContext<PicartchuContext>(
-    options => options. UseSqlServer(
-        builder. Configuration. GetConnectionString("PicartchuConnection")
+builder.Services.AddDbContext<PicartchuContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("PicartchuConnection")
     )
 );
+var app = builder. Build( );
+
+
 
 // Configure the HTTP request pipeline.
 if(!app. Environment. IsDevelopment( ))
