82a7cd780ef459027a96a360d8c8108a6eeb1588
HSIAO YU SHAN <amy90149810@gmail.com>
2026-08-20 16:26:47 +0800

    帶入資料

--- PokemonCard/Controllers/MyAccountController.cs
+++ PokemonCard/Controllers/MyAccountController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft. AspNetCore. Mvc;
+using PokemonCard. Models;
 
 namespace PokemonCard. Controllers
 {
     public class MyAccountController: Controller
     {
+        private readonly PicartchuContext _context;
+        public MyAccountController( PicartchuContext context )
+        {
+            _context = context;
+        }
         public IActionResult MemberCenter( )
         {
-            return View( );
+
+            var product = _context. Products. FirstOrDefault( );
+            var StoreName = _context. Sellers. FirstOrDefault( );
+
+            ViewBag. Product = product;
+            ViewBag. StoreName = StoreName;
+
+
+            return View();
         }
     }
 }
