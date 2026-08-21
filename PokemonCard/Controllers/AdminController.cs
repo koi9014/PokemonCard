42df4f0bf8ea203bed54dda9a95a26a77d6d0d94
Perry5124 <perry147258369@gmail.com>
2026-08-21 16:03:45 +0800

    暫存

---
+++ PokemonCard/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PokemonCard.Models;
+
+namespace PokemonCard.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly PicartchuContext _context;
+
+
+        public AdminController(PicartchuContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult AdminCenter()
+        {
+            return View();
+        }
+    }
+}
---
+++ PokemonCard/Controllers/AdminLoginController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using PokemonCard.Models;
+namespace PokemonCard.Controllers
+{
+    public class AdminLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
