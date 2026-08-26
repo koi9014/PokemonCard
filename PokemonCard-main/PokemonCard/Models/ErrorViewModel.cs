3b0503370952ab31f55a426161073c1d65893387
zxiiizz <qaz10787870@gmail.com>
2026-08-26 14:58:24 +0800

    remove duplicated PokemonCard-main folder

--- PokemonCard-main/PokemonCard/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PokemonCard. Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string. IsNullOrEmpty(RequestId);
-    }
-}
