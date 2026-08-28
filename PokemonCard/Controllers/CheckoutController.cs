5d2afad102133d3171f3bc4a0c329d43618296c5
HSIAO YU SHAN <amy90149810@gmail.com>
2026-08-28 14:51:39 +0800

    結帳靜態頁面

---
+++ PokemonCard/Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft. AspNetCore. Mvc;
+
+namespace PokemonCard. Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index( )
+        {
+            return View( );
+        }
+    }
+}
