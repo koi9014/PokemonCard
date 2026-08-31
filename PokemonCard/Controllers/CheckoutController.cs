114dd9cf8506432a7bd863db3be2122ef0f9dd6b
HSIAO YU SHAN <amy90149810@gmail.com>
2026-08-31 16:29:02 +0800

    訂單結帳功能新增

--- PokemonCard/Controllers/CheckoutController.cs
+++ PokemonCard/Controllers/CheckoutController.cs
@@ -1,12 +1,49 @@
 ﻿using Microsoft. AspNetCore. Mvc;
+using Microsoft. EntityFrameworkCore;
+using PokemonCard. Models;
 
 namespace PokemonCard. Controllers
 {
     public class CheckoutController : Controller
     {
-        public IActionResult Index( )
+        private readonly PicartchuContext _context;
+
+        public CheckoutController( PicartchuContext context )
         {
-            return View( );
+            _context = context;
         }
+        public async Task<IActionResult> Index(
+            int productId,
+            int specificationId,
+            int quantity )
+        {
+            var product = await _context. Products
+                . Include(p => p. ProductImages
+                    . OrderBy(x => x. ImageOrder))
+                . Include(p => p. ProductSpecs)
+                . Include(p => p. Seller)
+                    . ThenInclude(s => s. User)
+                . FirstOrDefaultAsync(p => p. ProductId == productId);
+
+            if(product == null)
+            {
+                return NotFound( );
+            }
+
+            var spec = product. ProductSpecs
+                . FirstOrDefault(x => x. SpecificationId == specificationId);
+
+            if(spec == null)
+            {
+                return NotFound( );
+            }
+
+            ViewBag. Spec = spec;
+            ViewBag. Quantity = quantity;
+
+            return View(product);
+        }
+
+
     }
 }
